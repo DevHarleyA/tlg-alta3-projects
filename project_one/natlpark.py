5ab010fa737aadb58d93800b0a8375b5926fa117
DevHarleyA <aharleydev@gmail.com>
2022-09-21 18:34:45 -0400

    began natl park questionnaire

---
+++ project_one/natlpark.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Welcome to the National Park Questionnaire! 
+Looking for a new national park trip but can't seem to choose from all the options? Take this questionnaire to determine where your next national park vacation will be!
+
+Project One submission by A.Harley for TLG-ALTA3"""
+
+def main():
+
+    # create a list that will contain all of the possible answers
+    # consider turning into a dictionary with the park name as key, and brief description as value (?)
+    parks = ["Yosemite National Park", "Zion National Park", "Wind Cave National Park", "Virgin Islands National Park"]
+
+    # Q1: What is your favorite season? (winter, summer, spring, autumn)
+
+    # Q2: Are you afraid of Bears? (Y/N)
+
+    # Q3: Do you prefer the East Coast or the West Coast?
+
+    # Q4:
+
+
+
+
+
+
+
+
+main()
